3b5a11f2055d6f038d249fec0da7291d086ccafc
Fredi1574 <Fredi1574@gmail.com>
2025-03-26 21:57:19 +0200

    Added page title

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,10 @@
 import CardsGrid from "@/components/CardsGrid";
 
+export const metadata = {
+  title: "movie_tracker",
+  description: "movie_tracker",
+};
+
 export default async function Home() {
   return (
     <div className="container mx-auto p-4">
